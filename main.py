c5491287626c4e6dadb0a90428f074ca694da4f5
Paul Khv <paul.khvaliuk@icloud.com>
2022-12-30 19:31:32 +0200

    updated parse_cookie function + tests added

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import requests
+
 def parse(query: str) -> dict:
     return {}
 
@@ -11,10 +13,14 @@ if __name__ == '__main__':
 
 
 def parse_cookie(query: str) -> dict:
-    return {}
+    return {"cookie": False}
 
 
 if __name__ == '__main__':
+    assert parse_cookie('name=Dima;') == {'name': 'Dima'}
+    assert parse_cookie('') == {}
+    assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
+    assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
     assert parse_cookie('name=Dima;') == {'name': 'Dima'}
     assert parse_cookie('') == {}
     assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
