1d960b7b97916c4438538db32e00947d892c61ee
Paul Khv <paul.khvaliuk@icloud.com>
2022-12-30 19:29:34 +0200

    updated parse function + tests added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def parse(query: str) -> dict:
-    return {}
+    return {"result": True}
 
 
 if __name__ == '__main__':
@@ -7,6 +7,10 @@ if __name__ == '__main__':
     assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
     assert parse('http://example.com/') == {}
     assert parse('http://example.com/?') == {}
+    assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
+    assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
+    assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
+    assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
     assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
 
 
